d6dad5e3a7cee8eb1def11cd2ddcd1eec4f0981d
TheOrlangur <theorlangur@gmail.com>
2026-01-09 09:56:47 +0100

    added submodules

---
+++ git_init_submodules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git submodule add git@github.com:theorlangur/esp_generic_lib.git submodules/esp_generic_lib
+git submodule add git@github.com:theorlangur/nrf_zb_cpp.git submodules/nrf_zb_cpp
+git submodule add git@github.com:theorlangur/nrf_general.git submodules/nrf_general
