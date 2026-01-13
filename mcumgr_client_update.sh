e218ef82785d107fa6ac435b5db537b8f68b5ec3
TheOrlangur <theorlangur@gmail.com>
2026-01-13 21:11:26 +0100

    increased timeout, using udev rule to reference usb-to-uart ch340

--- mcumgr_client_update.sh
+++ mcumgr_client_update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./mcumgr-client --nb-retry 16 -u 1000 -b 921600 -m 4096 -l 8192 -d /dev/ttyUSB0 upload build_ezurio_llvm/presence_multi_v2/zephyr/zephyr.signed.bin
+./mcumgr-client --nb-retry 16 -u 2000 -b 921600 -m 4096 -l 8192 -d /dev/nrf54l15 upload build_ezurio_llvm/presence_multi_v2/zephyr/zephyr.signed.bin
