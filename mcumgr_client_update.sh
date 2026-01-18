c1e9506e307b34b7f62652e17d1685965d990847
TheOrlangur <theorlangur@gmail.com>
2026-01-18 21:37:40 +0100

    still unstable... cranking up again some numbers...

--- mcumgr_client_update.sh
+++ mcumgr_client_update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./mcumgr-client --nb-retry 16 -u 2000 -b 921600 -m 4096 -l 8192 -d /dev/nrf54l15 upload build_ezurio_llvm/presence_multi_v2/zephyr/zephyr.signed.bin
+./mcumgr-client --nb-retry 24 -u 3000 -b 921600 -m 4096 -l 8192 -d /dev/nrf54l15 upload build_ezurio_llvm/presence_multi_v2/zephyr/zephyr.signed.bin
