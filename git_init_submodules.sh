144a52891cd83bbd5f47512a2a6bc402b76c394e
TheOrlangur <theorlangur@gmail.com>
2026-02-08 10:36:19 +0100

    added nrf_uart lib as a submodule

--- git_init_submodules.sh
+++ git_init_submodules.sh
@@ -2,3 +2,4 @@
 git submodule add git@github.com:theorlangur/esp_generic_lib.git submodules/esp_generic_lib
 git submodule add git@github.com:theorlangur/nrf_zb_cpp.git submodules/nrf_zb_cpp
 git submodule add git@github.com:theorlangur/nrf_general.git submodules/nrf_general
+git submodule add git@github.com:theorlangur/nrf_uart.git submodules/nrf_uart
