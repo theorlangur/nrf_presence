20ad0b9b565ff580ab27bcd8e75e3e9a5b3c93fc
TheOrlangur <theorlangur@gmail.com>
2026-07-13 21:52:11 +0200

    more adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -1215,24 +1215,25 @@ int main(void)
     }
 
     dev_ctx.occupancy.occupancy = false;
+    //TODO: method calling support as a handler
     /* Register callback for handling ZCL commands. */
     auto dev_cb = zbm::tpl_device_cb<
 	zbm::dev_cb_handlers_desc_t{ .error_handler = on_dev_cb_error }
 	//main instance
 	zbm::cb_handler_t{.id = ZB_ZCL_SET_ATTR_VALUE_CB_ID, .ep = ^^device_ctx_t::ep1, .target = kAttrBaseCfg, .handler = ^^on_set_base_config<ld2412_1>}
-	, zb::handle_set_for<kAttrBaseCfg,               &on_set_base_config<ld2412_1>>(zb_ep)
-	, zb::handle_set_for<kAttrLightSense,            &on_set_light_sense<ld2412_1>>(zb_ep)
-	, zb::handle_set_for<kAttrStillThr,              method_fwd<ld2412_1, &ld2412::Instance::set_still_thresholds_raw>>(zb_ep)
-	, zb::handle_set_for<kAttrMoveThr,               method_fwd<ld2412_1, &ld2412::Instance::set_move_thresholds_raw>>(zb_ep)
-	, zb::handle_set_for<kAttrStatWinSize,           method_fwd<ld2412_1, &ld2412::Instance::collect_statistics>>(zb_ep)
-	, zb::handle_set_for<kAttrBT,                    method_fwd<ld2412_1, &ld2412::Instance::switch_bluetooth>>(zb_ep)
+	,zbm::cb_handler_t{.id = ZB_ZCL_SET_ATTR_VALUE_CB_ID, .ep = ^^device_ctx_t::ep1, .target = kAttrLightSense, .handler = ^^on_set_light_sense<ld2412_1>}
+	,zbm::cb_handler_t{.id = ZB_ZCL_SET_ATTR_VALUE_CB_ID, .ep = ^^device_ctx_t::ep1, .target = kAttrStillThr, .handler = ^^on_set_light_sense<ld2412_1>}
+	//, zb::handle_set_for<kAttrStillThr,              method_fwd<ld2412_1, &ld2412::Instance::set_still_thresholds_raw>>(zb_ep)
+	//, zb::handle_set_for<kAttrMoveThr,               method_fwd<ld2412_1, &ld2412::Instance::set_move_thresholds_raw>>(zb_ep)
+	//, zb::handle_set_for<kAttrStatWinSize,           method_fwd<ld2412_1, &ld2412::Instance::collect_statistics>>(zb_ep)
+	//, zb::handle_set_for<kAttrBT,                    method_fwd<ld2412_1, &ld2412::Instance::switch_bluetooth>>(zb_ep)
 	//aux instance
-	, zb::handle_set_for<kAttrBaseCfg,               &on_set_base_config<ld2412_2>>(zb_ep_aux)
-	, zb::handle_set_for<kAttrLightSense,            &on_set_light_sense<ld2412_2>>(zb_ep_aux)
-	, zb::handle_set_for<kAttrStillThr,              method_fwd<ld2412_2, &ld2412::Instance::set_still_thresholds_raw>>(zb_ep_aux)
-	, zb::handle_set_for<kAttrMoveThr,               method_fwd<ld2412_2, &ld2412::Instance::set_move_thresholds_raw>>(zb_ep_aux)
-	, zb::handle_set_for<kAttrStatWinSize,           method_fwd<ld2412_2, &ld2412::Instance::collect_statistics>>(zb_ep_aux)
-	, zb::handle_set_for<kAttrBT,                    method_fwd<ld2412_2, &ld2412::Instance::switch_bluetooth>>(zb_ep_aux)
+	,zbm::cb_handler_t{.id = ZB_ZCL_SET_ATTR_VALUE_CB_ID, .ep = ^^device_ctx_t::ep2, .target = kAttrBaseCfg, .handler = ^^on_set_base_config<ld2412_2>}
+	,zbm::cb_handler_t{.id = ZB_ZCL_SET_ATTR_VALUE_CB_ID, .ep = ^^device_ctx_t::ep2, .target = kAttrLightSense, .handler = ^^on_set_light_sense<ld2412_2>}
+	//, zb::handle_set_for<kAttrStillThr,              method_fwd<ld2412_2, &ld2412::Instance::set_still_thresholds_raw>>(zb_ep_aux)
+	//, zb::handle_set_for<kAttrMoveThr,               method_fwd<ld2412_2, &ld2412::Instance::set_move_thresholds_raw>>(zb_ep_aux)
+	//, zb::handle_set_for<kAttrStatWinSize,           method_fwd<ld2412_2, &ld2412::Instance::collect_statistics>>(zb_ep_aux)
+	//, zb::handle_set_for<kAttrBT,                    method_fwd<ld2412_2, &ld2412::Instance::switch_bluetooth>>(zb_ep_aux)
     >;
 
     ZB_ZCL_REGISTER_DEVICE_CB(dev_cb);
