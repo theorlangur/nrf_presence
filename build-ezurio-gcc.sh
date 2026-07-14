dc6c04c97448a7290edf6142749aa53b612d8b15
TheOrlangur <theorlangur@gmail.com>
2026-07-14 22:20:02 +0200

    brought back gcc16.1 build scripts

---
+++ build-ezurio-gcc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /home/theorlangur/ncs/toolchains/7cbc0036f4/gcc16_cross_compile_env.sh
+west build --build-dir build_ezurio_gcc16 . --pristine \
+    --board orlangur_ezurio_nrf54l15/nrf54l15/cpuapp -- \
+    -DBOARD_ROOT=~/myapps/cpp/nrf \
+    -DCONF_FILE="prj.conf config/cpp_gcc16.conf config/nrf54l15.conf config/zb.conf config/no_log.conf" \
+    -DZEPHYR_TOOLCHAIN_VARIANT=cross-compile \
+    -DCMAKE_TOOLCHAIN_FILE=/home/theorlangur/ncs/toolchains/7cbc0036f4/gcc16_cross_compile.cmake
