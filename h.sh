70e5af1d550e52a9487a2312b8a01aab2ff551c6
TheOrlangur <theorlangur@gmail.com>
2026-08-14 10:29:04 +0200

    buildable with 1.4.0 (no LTO though)

--- h.sh
+++ h.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+<<<<<<< HEAD
 source ~/ncs/toolchains/7cbc0036f4/gcc16_env.sh
+=======
+source /home/theorlangur/ncs/toolchains/fbf7391cab/gcc16_env.sh
+>>>>>>> 6792d23 (buildable with 1.4.0 (no LTO though))
 $*
