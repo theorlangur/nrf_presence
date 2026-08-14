b2434afec8587fdaf0635a484750b5ae89dedd0b
TheOrlangur <theorlangur@gmail.com>
2026-08-14 11:12:46 +0200

    fixed building/env scripts

--- h.sh
+++ h.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-source ~/ncs/toolchains/7cbc0036f4/gcc16_env.sh
-=======
 source /home/theorlangur/ncs/toolchains/fbf7391cab/gcc16_env.sh
->>>>>>> 6792d23 (buildable with 1.4.0 (no LTO though))
 $*
