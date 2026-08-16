d69c10ce0091954b871fba9be5d44b98b7603e46
TheOrlangur <theorlangur@gmail.com>
2026-08-16 21:57:56 +0200

    added script to run bloaty

---
+++ bloaty_cmp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bloaty -d fullsymbols -n 0 ~/myapps/cpp/nrf/zb_1.4.0/app/presence_multi_v2/build_ezurio_llvm/presence_multi_v2/zephyr/zephyr.elf -- ~/myapps/cpp/nrf/zbvscode/app/presence_multi_v2/build_ezurio_llvm/presence_multi_v2/zephyr/zephyr.elf
